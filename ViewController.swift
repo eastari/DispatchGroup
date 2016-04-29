c7e8bd26a43070dc859f71da79a2e10d73eb8c0b
Evgeny Starikov <starikovea@yandex.ua>
2016-04-29 19:51:40 +0300

    2-d commit

--- ViewController.swift
+++ ViewController.swift
@@ -14,12 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var images = [UIImage]()
+    var downLoadTimer: NSTimer!
+    let timeUpdateInterval: Int = 3
+    var secondsToUpdate: Int = 3
+    let thePaths = ["http://easyways.com.ua/storage/gallery/images/tours/bde/bdef63df38a15170593a6ebecaac1e41.jpg", "http://mywishlist.ru/pic/i/wish/orig/004/719/410.jpeg", "http://www.bontravel.com.ua/wp-content/uploads/2014/11/vodorosli.jpg", "http://vybratpravilno.ru/wp-content/uploads/2014/12/148.jpg", "http://abyss.com.ua/wp-content/uploads/2015/04/daiving.jpg"]
+    let thePaths2 = ["http://seriobus.com/sites/default/files/britanskaya_korotkosherstnaya_koshka_6.jpg", "http://avivas.ru/img/2015/05/topic/38638/58036.jpg", "http://allphoto.in.ua/photo/23/es2236596.jpg", "http://www.fullhdoboi.ru/_ph/29/720623227.jpg", "http://www.wallpaper-wallpaper.com/wp-content/uploads/images/cats-dogs/cats-dogs-wallpaper-retina-hd-download-18.jpg"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let thePaths = ["http://easyways.com.ua/storage/gallery/images/tours/bde/bdef63df38a15170593a6ebecaac1e41.jpg", "http://mywishlist.ru/pic/i/wish/orig/004/719/410.jpeg", "http://www.bontravel.com.ua/wp-content/uploads/2014/11/vodorosli.jpg", "http://vybratpravilno.ru/wp-content/uploads/2014/12/148.jpg", "http://abyss.com.ua/wp-content/uploads/2015/04/daiving.jpg"]
+        
         self.downloadPhotosWithURLs(thePaths)
+        self.setupTimer()
         
     }
     
@@ -74,6 +81,29 @@ class ViewController: UIViewController {
         return cell
         
     }
+    
+    // MARK: Timer
+    
+    func setupTimer() {
+        if downLoadTimer == nil  {
+            let theSelector: Selector = "updateDownloadTimer:"
+            downLoadTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: theSelector, userInfo: nil, repeats: true)
+        }
+    }
+    
+    
+    func updateDownloadTimer(timer: NSTimer) {
+        self.secondsToUpdate -= 1
+        print(self.secondsToUpdate)
+        if self.secondsToUpdate == 0 {
+            self.secondsToUpdate = timeUpdateInterval
+            self.downloadPhotosWithURLs(thePaths2)
+        }
+    }
+    
+    
+    
+    
 
 }
 
